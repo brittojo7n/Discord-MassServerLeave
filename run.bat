a29eaba191f9fef286df92215667b615903e79f8
brittojo7n <sjbritto2003@mail.com>
2025-09-30 12:24:03 +0530

    commit

--- run.bat
+++ run.bat
@@ -1,4 +1,9 @@
 @echo off
+IF NOT EXIST venv\ (
+    python -m venv venv
+)
+call venv\Scripts\activate
+
 python -c "import requests" 
 IF %ERRORLEVEL% NEQ 0 (
     ECHO Installing missing requirements...
